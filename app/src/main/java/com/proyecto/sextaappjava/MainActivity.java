6fba37bebf767ec2af2ef62b82ed54d0d525e674
Juan Medina <juandavidds08@gmail.com>
2021-11-18 21:39:37 -0500

    Validaciones

--- app/src/main/java/com/proyecto/sextaappjava/MainActivity.java
+++ app/src/main/java/com/proyecto/sextaappjava/MainActivity.java
@@ -7,13 +7,17 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int op=0;
     LinearLayout CONTE;
     Fragment FRAG;
-    Button B1;
+    Button B1,B2;
+    EditText E1,E2,E3;
+    TextView T1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         CONTE=(LinearLayout)findViewById(R.id.contenedor);
         CONTE.setVisibility(View.INVISIBLE);
         B1=(Button)findViewById(R.id.b1);
+        B2=(Button)findViewById(R.id.b2);
+        E1=(EditText) findViewById(R.id.e1);
+        E2=(EditText) findViewById(R.id.e2);
+        E3=(EditText) findViewById(R.id.e3);
+        T1=(TextView) findViewById(R.id.t1);
+
     }
 
 
@@ -52,5 +62,52 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction=manager.beginTransaction();
         transaction.replace(R.id.fragment,frag);
         transaction.commit();
+    }
+
+    public void guardarDatos(View view) {
+        String nombre, apellido,telefono;
+        int v1,v2,v3;
+        //Lectura
+        nombre=E1.getText().toString();
+        apellido=E2.getText().toString();
+        telefono=E3.getText().toString();
+        //validacion
+        v1=validarDatos(1,nombre);
+        v2=validarDatos(1,apellido);
+        v3=validarDatos(2,telefono);
+        if (v1!=1 || v2!=1 || v3!=1){
+
+            T1.setText("error");
+        }
+        else{
+            T1.setText("ok");
+        }
+        //almacenamiento
+    }
+
+    private int validarDatos(int i, String cadena) {
+        int  validacion =1,c,pos;
+        switch (i){
+            case 1:
+                for (pos=0; pos < cadena.length(); pos=pos+1){
+                    c=cadena.charAt(pos);
+                    if (c<67||c>90){
+                        validacion=0;
+                        pos=cadena.length();
+                    }
+                }
+            break;
+
+            case 2:
+                for (pos=0; pos < cadena.length(); pos=pos+1){
+                    c=cadena.charAt(pos);
+                    if (c<48||c>57){
+                        validacion=0;
+                        pos=cadena.length();
+                    }
+                }
+                break;
+        }
+        return validacion;
     }
 }
